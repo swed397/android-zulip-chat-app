6c4f7adb3b584a547980d3caa4ac9a9bedfcb3f5
Rodion <swed197@mail.ru>
2023-12-07 01:17:10 +0500

    add test api + start added dagger to di

--- app/src/main/java/com/android/zulip/chat/app/data/network/RetrofitClientInstance.kt
+++ app/src/main/java/com/android/zulip/chat/app/data/network/RetrofitClientInstance.kt
@@ -2,7 +2,6 @@ package com.android.zulip.chat.app.data.network
 
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
