129b6e1f2bade8b6d5e41da2efe41ce16cced157
Rodion <swed197@mail.ru>
2023-12-22 18:46:54 +0500

    some refactoring + started realise logic with open streams

--- app/src/main/java/com/android/zulip/chat/app/ui/chanels/StreamUiModel.kt
+++ app/src/main/java/com/android/zulip/chat/app/ui/chanels/StreamUiModel.kt
@@ -5,6 +5,8 @@ import com.android.zulip.chat.app.data.network.model.TopicInfo
 data class StreamUiModel(
     val id: Long,
     val name: String,
+
+    //ToDo bad
     var isOpened: Boolean = true,
     val topicsList: List<TopicInfo>
 )
